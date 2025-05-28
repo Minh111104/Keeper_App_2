5bb45c552a912a7cd734dd7795419f6ff36e3392
Minh Nguyen <quangminh111104@gmail.com>
2025-05-28 11:31:24 +0700

    Updated code

--- src/dkeeper_assets/src/components/App.jsx
+++ src/dkeeper_assets/src/components/App.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import { dkeeper } from "../../../declarations/dkeeper";
+import { title } from "process";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
     setNotes(prevNotes => {
-      return [...prevNotes, newNote];
+      dkeeper.createNote(newNote.title, newNote.content);
+      return [newNote, ...prevNotes];
     });
   }
+
+  useEffect(() => {
+    console.log("useEffect called");
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const notesArray = await dkeeper.readNotes();
+    setNotes(notesArray);
+  };
 
   function deleteNote(id) {
     setNotes(prevNotes => {
